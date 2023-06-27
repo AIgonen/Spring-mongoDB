2b70accfc1d203f8bfddf56e73d73931d9122a64
Andrei <a.igonen>
2023-06-27 16:57:10 +0300

    a

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,17 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import lombok.Data;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+
+import javax.management.Query;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +20,32 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner runner(StudentRepository repository, MongoTemplate mongoTemplate) {
+		return args -> {
+			Address address = new Address("England", "London", "NE98");
+			String email = "post@post.com";
+			Student student = new Student("Toomas",
+					"Tompson",
+					email,
+					Gender.MALE,
+					address,
+					List.of("CS", "Math"),
+					BigDecimal.TEN,
+					LocalDateTime.now());
+
+			Query query = new Query();
+			query.addCriteria(Criteria.where("email").is(email));
+
+			List<Student> students = mongoTemplate.find(query, Student.class);
+
+			if (students.size() > 1) {
+				throw new IllegalStateException("found namy students with email" + email);
+			}
+			if (students.isEmpty()) {
+				repository.insert(student);
+			}
+
+		};
+	}
 }
